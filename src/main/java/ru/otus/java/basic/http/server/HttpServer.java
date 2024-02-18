fe2699bde6baa8bb31161ab201d43904f6dd9e97
Stbran <mistral-ya@yandex.ru>
2024-02-18 15:54:03 +0300

    FC: logging and threads pool

--- src/main/java/ru/otus/java/basic/http/server/HttpServer.java
+++ src/main/java/ru/otus/java/basic/http/server/HttpServer.java
@@ -3,10 +3,16 @@ package ru.otus.java.basic.http.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 public class HttpServer {
     private int port;
     private Dispatcher dispatcher;
+    private ExecutorService executorService;
+    private static final Logger logger = LogManager.getLogger(HttpServer.class.getName());
+
 
     public HttpServer(int port) {
         this.port = port;
@@ -15,20 +21,47 @@ public class HttpServer {
 
     public void start() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
-            System.out.println("Сервер запущен на порту: " + port);
+            logger.info("Сервер запущен на порту: " + port);
+            executorService = Executors.newFixedThreadPool(3);
             while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                    byte[] buffer = new byte[8192];
-                    int n = socket.getInputStream().read(buffer);
-                    String rawRequest = new String(buffer, 0, n);
-                    HttpRequest httpRequest = new HttpRequest(rawRequest);
-                    dispatcher.execute(httpRequest, socket.getOutputStream());
+                try {
+                    Socket socket = serverSocket.accept();
+                    executorService.execute(() -> {
+                        logger.info("Запущен поток " + Thread.currentThread().getName());
+                        try {
+                            poolStart(socket);
+                        } catch (IOException e) {
+                            logger.error("Ошибка при чтении из потока", e);
+                        }
+                    });
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    logger.error("Ошибка создания сокета", e);
                 }
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Ошибка инициализации serverSocket", e);
+        } finally {
+            logger.info("Закрытие пула потоков");
+            executorService.shutdown();
+        }
+    }
+
+
+    private void poolStart(Socket socket) throws IOException {
+        try {
+            byte[] buffer = new byte[8192];
+            int n = socket.getInputStream().read(buffer);
+            logger.info("Читаем входящее сообщение");
+            String rawRequest = new String(buffer, 0, n);
+            HttpRequest httpRequest = new HttpRequest(rawRequest);
+            logger.info("Начало исполнения запроса");
+            dispatcher.execute(httpRequest, socket.getOutputStream());
+            logger.info("Запрос исполнен");
+        } catch (IOException e) {
+            logger.error("Ошибка чтения из потока", e);
+        } finally {
+            logger.info("Закрываем сокет");
+            socket.close();
         }
     }
 }
